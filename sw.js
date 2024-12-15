ee2665c58cc8d47d1570d1fac2a334d811a6283b
xg <1192736652@qq.com>
2024-12-15 16:13:22 +0800

    Site updated: 2024-12-15 16:13:21

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1734248968896";
+const VERSION = "1734250387181";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
